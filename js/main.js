007ff1178f90380e013600414ee1dc5322444afa
Sandra Szenti <alexandraszenti@gmail.com>
2013-10-30 17:07:57 +0000

    comments added

--- js/main.js
+++ js/main.js
@@ -1,46 +1,20 @@
 var getKeywords = function (urlInput, current) {
-	company = current;
-	var tags = $("<span />")
-	.html("the address is " + urlInput);
-	$(company).append(tags);
-
+	var company = current;
 	
-	// $.ajax({
-	//     url: urlInput,
-	//     type: 'GET',
-	//     success: function(res) {
-	//         var metaKeywords = $(res.responseText).siblings("meta[name='Keywords']").attr("content");
-	//         var tags = $("<span />")
-	// 		.html(this.text);
-	// 		// .css("color", "red");
-	// 		$("#current").append(tags);
-	//     }
-	// });
+	$.ajax({		//make this call syncronous by setting async:false
+	    url: urlInput,
+	    type: 'GET',
+	    success: function(res) { //might need an error as well... remove from list?
+	        var metaKeywords = $(res.responseText).siblings("meta[name='Keywords']").attr("content");
+	        var tags = $("<span />")
+			.html(this.text);
+			// .css("color", "red");
+			$(company).append(tags);
+			//get description as well at the same time, maybe store keywords in an array?
+	    }
+	});
+
 }
-
-// var getDescription = function (urlInput) {
-
-// 	$.ajax({
-// 	    url: urlInput,
-// 	    type: 'GET',
-// 	    success: function(res) {
-// 	        var metaDescription = $(res.responseText).siblings("meta[name='Description']").attr("content");
-// 	        $("p").html(metaDescription);
-// 	    }
-// 	});
-// }
-
-
-
-// var companyRequest = function () {
-// 	for (var i = 0; i < 2; i++) {
-// 		console.log("in companyRequest");
-// 		var currentUrl = $(this).attr("href");
-// 		console.log(currentUrl);
-// 		getKeywords(currentUrl);
-// 	};
-	
-// }
 
 var listLinks = function () {
 	$.ajax({
@@ -49,37 +23,27 @@ var listLinks = function () {
 	    success: function(res) {
 	        var ahref = $(res.responseText).find("a");
 	        $(ahref).each(function( index ) {
-				// console.log( index + ": " + $(this).attr("href") );
 				if (index > 3) {
 					if (this.text != "(hiring)") {
-						var currentUrl = $(this).attr("href");
 						var company = $("<li />")
-						.html(this.text)
+						.html(this.text);
 						$(company).append($("<br />"));
-						// .css("color", "red");
-						//here comes the data enquiry
-						// var tags = $("<span />")
-						// .html("testing Keywords a lot of them. lorem ipsum. cross domain ajax jquery ajax other sort elements ignore etc.");
-						// $(company).append(tags);
 
-
-						// console.log(currentUrl);
+						var currentUrl = $(this).attr("href");
+						
 						getKeywords(currentUrl, company);
 						$("ul").append(company);
 						$(company).css("visibility", "visible");
 					};
 				};
 			});
-	        
 	    }
 	});
 }
 
 function initialise() {
 	console.log("initalisation started...");
-	// $('#container').load('http://www.vinylgrafik.hu'); // SERIOUSLY!
  	listLinks();
-	// getKeywords("http://www.vinylgrafik.hu");
 }
 
 $(document).ready(initialise);
