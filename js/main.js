282880e0ae700d8f4456b9f6250896c6722cc590
Sandra Szenti <alexandraszenti@gmail.com>
2013-11-24 14:22:40 -0500

    checking for undefined keywords

--- js/main.js
+++ js/main.js
@@ -1,15 +1,34 @@
+var addKeywords = function (metaKeywords) {
+	var tags = $("<span />")
+	.html("<p>"+metaKeywords+"</p>");
+	$(company).append(tags);
+	keywordCount = keywordCount+1;
+}
+
+//var keywordsArray = new Array ();
+
 var getKeywords = function (urlInput, current) {
 	var company = current;
 	
-	$.ajax({		//make this call syncronous by setting async:false
+	$.ajax({
 	    url: urlInput,
 	    type: 'GET',
 	    async: false,
-	    success: function(res) { //might need an error as well... remove from list? + check for undefined
-	        var metaKeywords = $(res.responseText).siblings("meta[name='Keywords']").attr("content");
-	        var tags = $("<span />")
-			.html("<p>"+metaKeywords+"</p>");
-			$(company).append(tags);
+	    success: function(res) { //might need an error as well... remove from list? 
+	        var metaKeywords = $(res.responseText).siblings("meta[name='keywords']").attr("content");
+	        
+	        if (metaKeywords === undefined) {
+	        	metaKeywords = $(res.responseText).siblings("meta[name='Keywords']").attr("content");
+
+	        }
+	        
+	        else {
+	        	var tags = $("<span />")
+				.html("<p>"+metaKeywords+"</p>");
+				$(company).append(tags);
+				keywordCount = keywordCount+1;
+	        };
+	        
 			//get description as well at the same time, maybe store keywords in an array?
 	    }
 	});
@@ -17,6 +36,10 @@ var getKeywords = function (urlInput, current) {
 }
 
 var listLinks = function () {
+
+	// the ajax call takes the links on the nytm page, and if it is a company name,
+	// it pushes it back to the website by displaying the name of the company.
+
 	$.ajax({
 	    url: "http://nytm.org/made-in-nyc",
 	    type: 'GET',
@@ -29,8 +52,12 @@ var listLinks = function () {
 						.html(this.text);
 						$(company).append($("<br />"));
 
+						// when it gets to a company url, it searches for the
+						// company's website. It wants to get the metadata of
+						// the with the getKeywords function.
+
 						var currentUrl = $(this).attr("href");
-						
+					
 						getKeywords(currentUrl, company);
 						
 						$("ul").append(company);
@@ -43,8 +70,11 @@ var listLinks = function () {
 }
 
 function initialise() {
+
 	console.log("initalisation started...");
+	// this function lists the companies from nytmeetup group.
  	listLinks();
+ 	// three_multiples(14);
 }
 
 $(document).ready(initialise);
